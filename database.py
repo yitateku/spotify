3535ee0a452b3f4ea449ec975b7e44c5c3c4778c
yitateku aderaw <ayitateku@abaybank.local>
2025-07-23 05:46:13 -0700

    update databae

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:root@localhost:5432/musicapp'
+DATABASE_URL = 'postgresql://postgres:PKciPzjzWKNhNRSeffSliNtQvtKzMWnZ@shortline.proxy.rlwy.net:11714/railway'
 
 
 engine = create_engine(DATABASE_URL)
