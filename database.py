21e306a15c01dcfee28b50a75021ef7b097d7f83
yitateku aderaw <ayitateku@abaybank.local>
2025-07-23 03:29:26 -0700

    update databae

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = 'postgresql://postgres:root@localhost:5432/musicapp'
 
-DATABASE_URL = 'postgresql://postgres.gwkwtinjhitwegdhfcxv:O5CcxrTjU7Jm9WjI@aws-0-eu-north-1.pooler.supabase.com:6543/postgres'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
